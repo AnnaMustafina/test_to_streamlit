b3e651bfc1f8a279ae6af3fdcdeff48f6a6a36e6
Mustafina An <148993537+AnnaMustafina@users.noreply.github.com>
2024-08-01 09:24:31 +0400

    Update to_test.py

--- to_test.py
+++ to_test.py
@@ -8,19 +8,18 @@ bot_token = "6647621334:AAG5CiIbxm07vSVV0XLuPFOgtRhdyClS1AE"
 webhook_url = "https://app-to-test-j3ojcxof7bphybygqqvkct.streamlit.app"
 
 # Получение данных о Telegram ID
-   def get_last_user_id():
+def get_last_user_id():
        response = requests.get('https://app-to-test-j3ojcxof7bphybygqqvkct.streamlit.app')
        if response.status_code == 200:
            return response.json()['user_id']
        else:
            return None
 
-   last_user_id = get_last_user_id()
-
-   if last_user_id:
-       st.write("Последний Telegram ID: " + str(last_user_id))
-   else:
-       st.write("Нет данных о Telegram ID")
+last_user_id = get_last_user_id()
+if last_user_id:
+   st.write("Последний Telegram ID: " + str(last_user_id))
+else:
+   st.write("Нет данных о Telegram ID")
   
 #if id_now =='IU13488':
 #    st.write('Привет, пользователь', id_now)
