5f9f0379fc52fed190db6148e7d07c741454d6d3
Mustafina An <148993537+AnnaMustafina@users.noreply.github.com>
2024-08-01 11:44:11 +0400

    Update to_test.py

--- to_test.py
+++ to_test.py
@@ -42,12 +42,12 @@ else:
 #webhook_url = "https://app-to-test-j3ojcxof7bphybygqqvkct.streamlit.app"
 
 
-chart_data = pd.DataFrame(
-    {
-        "col1": list(range(20)) * 3,
-        "col2": np.random.randn(60),
-        "col3": ["A"] * 20 + ["B"] * 20 + ["C"] * 20,
-    }
-)
+#chart_data = pd.DataFrame(
+#    {
+#        "col1": list(range(20)) * 3,
+#        "col2": np.random.randn(60),
+#        "col3": ["A"] * 20 + ["B"] * 20 + ["C"] * 20,
+#    }
+#)
 
-st.bar_chart(chart_data, x="col1", y="col2", color="col3")
+#st.bar_chart(chart_data, x="col1", y="col2", color="col3")
