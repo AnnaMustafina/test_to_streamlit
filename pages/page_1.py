70fe83f5772d70a432e27de6f7bf8c9b7f69539b
Mustafina An <148993537+AnnaMustafina@users.noreply.github.com>
2024-09-05 14:37:54 +0400

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+chart_data = pd.DataFrame(np.random.randn(20, 4), columns=["a", "b", "c"])
 
 st.line_chart(chart_data)
+
